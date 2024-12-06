6b7c7ecef468fcc16faae523e2901aa16ad8e961
Tan Aydemir <100552661+tan-aydemir@users.noreply.github.com>
2024-12-05 22:19:06 -0500

    Updated create_song_table.sql
    For the db, we'll only store the id and name, and we will get the weather information in real time in app

--- weather/sql/create_song_table.sql
+++ weather/sql/create_song_table.sql
@@ -1,12 +1,6 @@
-DROP TABLE IF EXISTS songs;
-CREATE TABLE songs (
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    artist TEXT NOT NULL,
-    title TEXT NOT NULL,
-    year INTEGER NOT NULL CHECK(year >= 1900),
-    genre TEXT NOT NULL,
-    duration INTEGER NOT NULL CHECK(duration > 0),
-    play_count INTEGER DEFAULT 0,
-    deleted BOOLEAN DEFAULT FALSE,
-    UNIQUE(artist, title, year)
-);
+    name TEXT NOT NULL UNIQUE,
+    deleted BOOLEAN DEFAULT FALSE
+)
